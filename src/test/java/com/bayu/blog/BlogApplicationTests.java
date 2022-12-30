517a0bb69e7f56ad26e71ea466afadb2ef4dcb8b
bayubagusbagaswara <bagaszwara12@gmail.com>
2022-12-30 08:56:29 +0700

    initial commit

---
+++ src/test/java/com/bayu/blog/BlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bayu.blog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
