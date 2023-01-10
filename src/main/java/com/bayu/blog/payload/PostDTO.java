06960d226b3c86841083d700aeca71e4aaf8c81f
bayubagusbagaswara <bagaszwara12@gmail.com>
2023-01-10 10:20:27 +0700

    add PostDTO

---
+++ src/main/java/com/bayu/blog/payload/PostDTO.java
@@ -0,0 +1,15 @@
+package com.bayu.blog.payload;
+
+import lombok.Data;
+
+@Data
+public class PostDTO {
+
+    private long id;
+
+    private String title;
+
+    private String description;
+
+    private String content;
+}
