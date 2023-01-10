e438636a4b842ec85580c21e268b58540538f36d
bayubagusbagaswara <bagaszwara12@gmail.com>
2023-01-10 10:39:00 +0700

    add method contract getAllPosts

--- src/main/java/com/bayu/blog/service/PostService.java
+++ src/main/java/com/bayu/blog/service/PostService.java
@@ -2,7 +2,11 @@ package com.bayu.blog.service;
 
 import com.bayu.blog.payload.PostDTO;
 
+import java.util.List;
+
 public interface PostService {
 
     PostDTO createPost(PostDTO postDTO);
+
+    List<PostDTO> getAllPosts();
 }
