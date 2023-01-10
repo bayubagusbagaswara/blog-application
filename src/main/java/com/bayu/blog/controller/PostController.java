f02a463676bff2f2bc173ba8993ba47e9f25719d
bayubagusbagaswara <bagaszwara12@gmail.com>
2023-01-10 10:35:06 +0700

    add method handler createPost

--- src/main/java/com/bayu/blog/controller/PostController.java
+++ src/main/java/com/bayu/blog/controller/PostController.java
@@ -1,4 +1,27 @@
 package com.bayu.blog.controller;
 
+import com.bayu.blog.payload.PostDTO;
+import com.bayu.blog.service.PostService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/posts")
 public class PostController {
+
+    private final PostService postService;
+
+    public PostController(PostService postService) {
+        this.postService = postService;
+    }
+
+    // create blog post
+    @PostMapping
+    public ResponseEntity<PostDTO> createPost(@RequestBody PostDTO postDTO) {
+        return new ResponseEntity<>(postService.createPost(postDTO), HttpStatus.CREATED);
+    }
 }
