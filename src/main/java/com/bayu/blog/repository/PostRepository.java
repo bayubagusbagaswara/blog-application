270014aeedaade0095eae0e1d7991638274fd99f
bayubagusbagaswara <bagaszwara12@gmail.com>
2023-01-10 10:15:59 +0700

    update PostRepository

--- src/main/java/com/bayu/blog/repository/PostRepository.java
+++ src/main/java/com/bayu/blog/repository/PostRepository.java
@@ -2,6 +2,9 @@ package com.bayu.blog.repository;
 
 import com.bayu.blog.entity.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface PostRepository extends JpaRepository<Post, Long> {
+
 }
