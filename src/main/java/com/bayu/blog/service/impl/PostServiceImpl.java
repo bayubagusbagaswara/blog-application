8c047e61ca4bd53fb0460a61ca4735468fcee3f2
bayubagusbagaswara <bagaszwara12@gmail.com>
2023-01-10 10:29:52 +0700

    implement method service createPost

--- src/main/java/com/bayu/blog/service/impl/PostServiceImpl.java
+++ src/main/java/com/bayu/blog/service/impl/PostServiceImpl.java
@@ -1,7 +1,40 @@
 package com.bayu.blog.service.impl;
 
+import com.bayu.blog.entity.Post;
+import com.bayu.blog.payload.PostDTO;
+import com.bayu.blog.repository.PostRepository;
+import com.bayu.blog.service.PostService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
-public class PostServiceImpl {
+public class PostServiceImpl implements PostService {
+
+    private final PostRepository postRepository;
+
+    @Autowired
+    public PostServiceImpl(PostRepository postRepository) {
+        this.postRepository = postRepository;
+    }
+
+    @Override
+    public PostDTO createPost(PostDTO postDTO) {
+        // convert DTO to entity
+        Post post = new Post();
+        post.setTitle(postDTO.getTitle());
+        post.setDescription(postDTO.getDescription());
+        post.setContent(postDTO.getContent());
+
+        Post newPost = postRepository.save(post);
+
+        // convert entity to DTO
+        PostDTO postResponse = new PostDTO();
+        postResponse.setId(newPost.getId());
+        postResponse.setTitle(newPost.getTitle());
+        postResponse.setDescription(newPost.getDescription());
+        postResponse.setContent(newPost.getContent());
+
+        return postResponse;
+    }
+
 }
