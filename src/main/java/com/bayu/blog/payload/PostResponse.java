2a1edf232351558b4524f115d6b167ebf6170056
bayubagusbagaswara <bagaszwara12@gmail.com>
2023-01-10 14:25:32 +0700

    add PostResponse

---
+++ src/main/java/com/bayu/blog/payload/PostResponse.java
@@ -0,0 +1,18 @@
+package com.bayu.blog.payload;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+public class PostResponse {
+
+    private List<PostDTO> content;
+    private int pageNo;
+    private int pageSize;
+    private long totalElements;
+    private int totalPages;
+    private boolean last;
+}
