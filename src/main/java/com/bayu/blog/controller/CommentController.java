cf146cf920416eb5bdef059b8543774fa23df9fe
bayubagusbagaswara <bagaszwara12@gmail.com>
2023-01-12 11:02:07 +0700

    add handler getCommentsByPostId

--- src/main/java/com/bayu/blog/controller/CommentController.java
+++ src/main/java/com/bayu/blog/controller/CommentController.java
@@ -6,6 +6,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/api/")
 public class CommentController {
@@ -22,5 +24,11 @@ public class CommentController {
 
         return new ResponseEntity<>(commentService.createComment(postId, commentDTO), HttpStatus.CREATED);
 
+    }
+
+    @GetMapping("/posts/{postId}/comments")
+    public List<CommentDTO> getCommentsByPostId(@PathVariable(value = "postId") Long postId) {
+
+        return commentService.getCommentsByPostId(postId);
     }
 }
