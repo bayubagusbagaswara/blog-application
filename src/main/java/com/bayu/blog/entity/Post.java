9295d596426c5119519e07a8ab1bded2d5e88846
bayubagusbagaswara <bagaszwara12@gmail.com>
2023-01-12 10:12:30 +0700

    add bidirectional to Comment

--- src/main/java/com/bayu/blog/entity/Post.java
+++ src/main/java/com/bayu/blog/entity/Post.java
@@ -5,6 +5,9 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.util.HashSet;
+import java.util.Set;
+
 @Entity
 @Table(name = "posts", uniqueConstraints = {
         @UniqueConstraint(name = "post_title_unique", columnNames = {"title"})
@@ -26,4 +29,9 @@ public class Post {
 
     @Column(name = "content", nullable = false)
     private String content;
+
+    @OneToMany(mappedBy = "post",
+            cascade = CascadeType.ALL,
+            orphanRemoval = true)
+    private Set<Comment> comments = new HashSet<>();
 }
