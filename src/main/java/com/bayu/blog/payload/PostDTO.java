e254091b3a383f8a800ce42c8f472ec3b6801174
bayubagusbagaswara <bagaszwara12@gmail.com>
2023-01-12 15:18:18 +0700

    add annotation for validation

--- src/main/java/com/bayu/blog/payload/PostDTO.java
+++ src/main/java/com/bayu/blog/payload/PostDTO.java
@@ -1,5 +1,7 @@
 package com.bayu.blog.payload;
 
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 import java.util.Set;
@@ -9,10 +11,21 @@ public class PostDTO {
 
     private long id;
 
+    // title should not be null or empty
+    // title should have at least 2 characters
+
+    @NotEmpty
+    @Size(min = 2, message = "Post title should have at least 2 characters")
     private String title;
 
+    // post description should be no null or empty
+    // post description should have at least 10 characters
+    @NotEmpty
+    @Size(min = 10, message = "Post description should have at least 10 characters")
     private String description;
 
+    // post content should not be null or empty
+    @NotEmpty
     private String content;
 
     private Set<CommentDTO> comments;
