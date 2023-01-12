1c801076c19833a947f9ef688f5ec6c745deb842
bayubagusbagaswara <bagaszwara12@gmail.com>
2023-01-12 10:59:14 +0700

    implement method getCommentsByPostId

--- src/main/java/com/bayu/blog/service/impl/CommentServiceImpl.java
+++ src/main/java/com/bayu/blog/service/impl/CommentServiceImpl.java
@@ -10,6 +10,9 @@ import com.bayu.blog.service.CommentService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 @Service
 public class CommentServiceImpl implements CommentService {
 
@@ -37,6 +40,16 @@ public class CommentServiceImpl implements CommentService {
         Comment newComment = commentRepository.save(comment);
 
         return mapToDTO(newComment);
+    }
+
+    @Override
+    public List<CommentDTO> getCommentsByPostId(long postId) {
+        // retrieve comments by postId
+        List<Comment> comments = commentRepository.findByPostId(postId);
+
+        return comments.stream()
+                .map(this::mapToDTO)
+                .collect(Collectors.toList());
     }
 
     private CommentDTO mapToDTO(Comment comment) {
