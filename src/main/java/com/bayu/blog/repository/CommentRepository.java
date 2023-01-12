823f9293ddd0a4022b62cb9d4e9916ca6f073ba3
bayubagusbagaswara <bagaszwara12@gmail.com>
2023-01-12 10:55:37 +0700

    add method findByPostId and getCommentsByPostId

--- src/main/java/com/bayu/blog/repository/CommentRepository.java
+++ src/main/java/com/bayu/blog/repository/CommentRepository.java
@@ -1,8 +1,14 @@
 package com.bayu.blog.repository;
 
 import com.bayu.blog.entity.Comment;
+import com.bayu.blog.payload.CommentDTO;
 import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
 
 public interface CommentRepository extends JpaRepository<Comment, Long> {
 
+    List<Comment> findByPostId(long postId);
+
+    List<CommentDTO> getCommentsByPostId(long postId);
 }
