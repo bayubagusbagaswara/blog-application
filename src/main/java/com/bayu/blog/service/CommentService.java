2c5e236a720d0ae8783822b226365bbba99f0885
bayubagusbagaswara <bagaszwara12@gmail.com>
2023-01-12 10:56:03 +0700

    add method contract getCommentsByPostId

--- src/main/java/com/bayu/blog/service/CommentService.java
+++ src/main/java/com/bayu/blog/service/CommentService.java
@@ -2,8 +2,12 @@ package com.bayu.blog.service;
 
 import com.bayu.blog.payload.CommentDTO;
 
+import java.util.List;
+
 public interface CommentService {
 
     CommentDTO createComment(long postId, CommentDTO commentDTO);
+
+    List<CommentDTO> getCommentsByPostId(long postId);
 
 }
