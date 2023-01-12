de4541cef97fbea9cb3b90c300e80678bcce3d5b
bayubagusbagaswara <bagaszwara12@gmail.com>
2023-01-12 10:11:38 +0700

    add model Comment

---
+++ src/main/java/com/bayu/blog/entity/Comment.java
@@ -0,0 +1,36 @@
+package com.bayu.blog.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "comments")
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Comment {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "email")
+    private String email;
+
+    @Column(name = "body")
+    private String body;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "post_id",
+            foreignKey = @ForeignKey(name = "fk_comment_post_id"),
+            referencedColumnName = "id",
+            nullable = false)
+    private Post post;
+}
