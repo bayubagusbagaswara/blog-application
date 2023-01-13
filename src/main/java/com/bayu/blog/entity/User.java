657633d3065b47ab19029b008adcd547c31fe04a
bayubagusbagaswara <bagaszwara12@gmail.com>
2023-01-13 09:42:15 +0700

    add model User

---
+++ src/main/java/com/bayu/blog/entity/User.java
@@ -0,0 +1,32 @@
+package com.bayu.blog.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "users", uniqueConstraints = {
+        @UniqueConstraint(columnNames = {"username"}, name = "user_username_unique"),
+        @UniqueConstraint(columnNames = {"email"}, name = "user_email_unique")
+})
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    @Column(nullable = false, unique = true)
+    private String username;
+    @Column(nullable = false, unique = true)
+    private String email;
+    @Column(nullable = false)
+    private String password;
+}
