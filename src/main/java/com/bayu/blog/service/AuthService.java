630b5864fec7cf7279ef2db1e76ea2e44fa878b4
bayubagusbagaswara <bagaszwara12@gmail.com>
2023-01-13 14:25:26 +0700

    add method contract register

--- src/main/java/com/bayu/blog/service/AuthService.java
+++ src/main/java/com/bayu/blog/service/AuthService.java
@@ -1,9 +1,12 @@
 package com.bayu.blog.service;
 
 import com.bayu.blog.payload.LoginDTO;
+import com.bayu.blog.payload.RegisterDTO;
 
 public interface AuthService {
 
     String login(LoginDTO loginDTO);
+
+    String register(RegisterDTO registerDTO);
 
 }
