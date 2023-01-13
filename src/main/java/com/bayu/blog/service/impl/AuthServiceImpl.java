8e63281d41fbf76cbe14d8cf4e208894412a490c
bayubagusbagaswara <bagaszwara12@gmail.com>
2023-01-13 14:38:09 +0700

    implement method register

--- src/main/java/com/bayu/blog/service/impl/AuthServiceImpl.java
+++ src/main/java/com/bayu/blog/service/impl/AuthServiceImpl.java
@@ -1,20 +1,37 @@
 package com.bayu.blog.service.impl;
 
+import com.bayu.blog.entity.Role;
+import com.bayu.blog.entity.User;
+import com.bayu.blog.exception.BlogApiException;
 import com.bayu.blog.payload.LoginDTO;
+import com.bayu.blog.payload.RegisterDTO;
+import com.bayu.blog.repository.RoleRepository;
+import com.bayu.blog.repository.UserRepository;
 import com.bayu.blog.service.AuthService;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import java.util.HashSet;
+import java.util.Set;
 
 @Service
 public class AuthServiceImpl implements AuthService {
 
     private final AuthenticationManager authenticationManager;
+    private final UserRepository userRepository;
+    private final RoleRepository roleRepository;
+    private final PasswordEncoder passwordEncoder;
 
-    public AuthServiceImpl(AuthenticationManager authenticationManager) {
+    public AuthServiceImpl(AuthenticationManager authenticationManager, UserRepository userRepository, RoleRepository roleRepository, PasswordEncoder passwordEncoder) {
         this.authenticationManager = authenticationManager;
+        this.userRepository = userRepository;
+        this.roleRepository = roleRepository;
+        this.passwordEncoder = passwordEncoder;
     }
 
     @Override
@@ -28,6 +45,34 @@ public class AuthServiceImpl implements AuthService {
         SecurityContextHolder.getContext().setAuthentication(authentication);
 
         return "User Logged-in successfully!.";
+    }
+
+    @Override
+    public String register(RegisterDTO registerDTO) {
+        // add check for username exists in database
+        if (userRepository.existsByUsername(registerDTO.getUsername())) {
+            throw new BlogApiException(HttpStatus.BAD_REQUEST, "Username is already exists!");
+        }
+
+        // add check for email exists in database
+        if (userRepository.existsByEmail(registerDTO.getEmail())) {
+            throw new BlogApiException(HttpStatus.BAD_REQUEST, "Email is already exists!");
+        }
+
+        User user = new User();
+        user.setName(registerDTO.getName());
+        user.setUsername(registerDTO.getUsername());
+        user.setEmail(registerDTO.getEmail());
+        user.setPassword(passwordEncoder.encode(registerDTO.getPassword()));
+
+        Set<Role> roles = new HashSet<>();
+        Role userRole = roleRepository.findByName("ROLE_USER").get();
+        roles.add(userRole);
+        user.setRoles(roles);
+
+        userRepository.save(user);
+
+        return "User registered successfully";
     }
 
 }
