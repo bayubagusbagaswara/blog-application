b2be8e55cfd3e5e521b10857d41b49d6290dc769
bayubagusbagaswara <bagaszwara12@gmail.com>
2023-01-13 14:24:32 +0700

    add AuthController

---
+++ src/main/java/com/bayu/blog/controller/AuthController.java
@@ -0,0 +1,31 @@
+package com.bayu.blog.controller;
+
+import com.bayu.blog.payload.LoginDTO;
+import com.bayu.blog.service.AuthService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/auth")
+public class AuthController {
+
+    private final AuthService authService;
+
+    public AuthController(AuthService authService) {
+        this.authService = authService;
+    }
+
+    // Build Login REST API
+    @PostMapping(value = {"/login", "/signin"})
+    public ResponseEntity<String> login(@RequestBody LoginDTO loginDTO) {
+        String response = authService.login(loginDTO);
+        return ResponseEntity.ok(response);
+    }
+
+
+
+}
