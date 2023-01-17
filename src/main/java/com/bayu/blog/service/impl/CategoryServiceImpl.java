01f7e197ba5c0e6ca315d2c9c22bbb15151f9138
bayubagusbagaswara <bagaszwara12@gmail.com>
2023-01-17 10:03:59 +0700

    add service implementation CategoryServiceImpl

---
+++ src/main/java/com/bayu/blog/service/impl/CategoryServiceImpl.java
@@ -0,0 +1,14 @@
+package com.bayu.blog.service.impl;
+
+import com.bayu.blog.payload.CategoryDTO;
+import com.bayu.blog.service.CategoryService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CategoryServiceImpl implements CategoryService {
+
+    @Override
+    public CategoryDTO addCategory(CategoryDTO categoryDTO) {
+        return null;
+    }
+}
