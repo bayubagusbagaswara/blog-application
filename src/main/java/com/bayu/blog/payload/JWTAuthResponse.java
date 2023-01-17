52a163c7e4eb31d2fa7dab78aa18c6e7f156fd22
bayubagusbagaswara <bagaszwara12@gmail.com>
2023-01-17 09:03:49 +0700

    add payload JWTAuthResponse

---
+++ src/main/java/com/bayu/blog/payload/JWTAuthResponse.java
@@ -0,0 +1,17 @@
+package com.bayu.blog.payload;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class JWTAuthResponse {
+
+    private String accessToken;
+    private final String tokenType = "Bearer";
+
+}
