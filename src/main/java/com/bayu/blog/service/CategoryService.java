4de6b093cc22e5d9fc6a2e08b4580713bad30fa8
bayubagusbagaswara <bagaszwara12@gmail.com>
2023-01-17 10:33:56 +0700

    add method contract getCategoryById

--- src/main/java/com/bayu/blog/service/CategoryService.java
+++ src/main/java/com/bayu/blog/service/CategoryService.java
@@ -5,4 +5,6 @@ import com.bayu.blog.payload.CategoryDTO;
 public interface CategoryService {
 
     CategoryDTO addCategory(CategoryDTO categoryDTO);
+
+    CategoryDTO getCategoryById(Long categoryId);
 }
