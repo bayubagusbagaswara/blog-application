4af7ca0e8e4f573d97886f6845261cdccf850104
bayubagusbagaswara <bagaszwara12@gmail.com>
2023-01-17 10:17:33 +0700

    implement method addCategory

--- src/main/java/com/bayu/blog/service/impl/CategoryServiceImpl.java
+++ src/main/java/com/bayu/blog/service/impl/CategoryServiceImpl.java
@@ -1,14 +1,27 @@
 package com.bayu.blog.service.impl;
 
+import com.bayu.blog.entity.Category;
 import com.bayu.blog.payload.CategoryDTO;
+import com.bayu.blog.repository.CategoryRepository;
 import com.bayu.blog.service.CategoryService;
+import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class CategoryServiceImpl implements CategoryService {
+
+    private final CategoryRepository categoryRepository;
+    private final ModelMapper modelMapper;
 
     @Override
     public CategoryDTO addCategory(CategoryDTO categoryDTO) {
-        return null;
+
+        Category category = modelMapper.map(categoryDTO, Category.class);
+        Category savedCategory = categoryRepository.save(category);
+
+        return modelMapper.map(savedCategory, CategoryDTO.class);
     }
+
 }
