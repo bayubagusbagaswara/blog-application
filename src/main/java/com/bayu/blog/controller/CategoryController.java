63b0efaf4036d9c036e0c768013c848cd1e3a23a
bayubagusbagaswara <bagaszwara12@gmail.com>
2023-01-17 10:39:34 +0700

    add method getCategory

--- src/main/java/com/bayu/blog/controller/CategoryController.java
+++ src/main/java/com/bayu/blog/controller/CategoryController.java
@@ -5,10 +5,7 @@ import com.bayu.blog.service.CategoryService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/categories")
@@ -27,5 +24,12 @@ public class CategoryController {
         CategoryDTO savedCategory = categoryService.addCategory(categoryDTO);
 
         return new ResponseEntity<>(savedCategory, HttpStatus.CREATED);
+    }
+
+    // Build Get Category REST API
+    @GetMapping("/{id}")
+    public ResponseEntity<CategoryDTO> getCategory(@PathVariable(value = "id") Long categoryId) {
+        CategoryDTO category = categoryService.getCategoryById(categoryId);
+        return ResponseEntity.ok(category);
     }
 }
