07a43e9742f9b1983287b96d0b149b1045bd253f
bayubagusbagaswara <bagaszwara12@gmail.com>
2023-01-17 09:56:14 +0700

    add model Category

---
+++ src/main/java/com/bayu/blog/entity/Category.java
@@ -0,0 +1,32 @@
+package com.bayu.blog.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.List;
+
+@Entity
+@Table(name = "categories")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "description")
+    private String description;
+
+    // bidirectional
+    @OneToMany(mappedBy = "category", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Post> posts;
+}
