61b4117db380744e6e4cfb9c36d08f22f60bcc94
bayubagusbagaswara <bagaszwara12@gmail.com>
2023-01-20 16:41:09 +0700

    update method updatePost

--- src/main/java/com/bayu/blog/service/impl/PostServiceImpl.java
+++ src/main/java/com/bayu/blog/service/impl/PostServiceImpl.java
@@ -97,9 +97,14 @@ public class PostServiceImpl implements PostService {
         Post post = postRepository.findById(id)
                 .orElseThrow(() -> new ResourceNotFoundException("Post", "id", id));
 
+        Category category = categoryRepository.findById(postDTO.getCategoryId())
+                .orElseThrow(() -> new ResourceNotFoundException("Category", "id", postDTO.getCategoryId()));
+
+
         post.setTitle(postDTO.getTitle());
         post.setDescription(postDTO.getDescription());
         post.setContent(postDTO.getContent());
+        post.setCategory(category);
 
         Post save = postRepository.save(post);
 
